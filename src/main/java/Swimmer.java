012627baee6dd9b54c909da7e50359da6e93a277
Jeremy Chen <jeremychenczm@gmail.com>
2026-03-18 21:26:24 -0700

    feat:lab07 q2q3

---
+++ src/main/java/Swimmer.java
@@ -0,0 +1,58 @@
+import java.util.List;
+
+public class Swimmer implements Comparable<Swimmer> {
+  private String name;
+  private List<Double> butterfly50mTimes;
+  private List<Double> backstroke50mTimes;
+  private List<Double> breaststroke50mTimes;
+  private List<Double> freestyle50mTimes;
+
+  public Swimmer(String name,
+                  List<Double> butterfly50mTimes,
+                  List<Double> backstroke50mTimes,
+                  List<Double> breaststroke50mTimes,
+                  List<Double> freestyle50mTimes) {
+    this.name = name;
+    this.butterfly50mTimes = butterfly50mTimes;
+    this.backstroke50mTimes = backstroke50mTimes;
+    this.breaststroke50mTimes = breaststroke50mTimes;
+    this.freestyle50mTimes = freestyle50mTimes;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<Double> getButterfly50mTimes() {
+    return butterfly50mTimes;
+  }
+
+  public List<Double> getBackstroke50mTimes() {
+    return backstroke50mTimes;
+  }
+
+  public List<Double> getBreaststroke50mTimes() {
+    return breaststroke50mTimes;
+  }
+
+  public List<Double> getFreestyle50mTimes() {
+    return freestyle50mTimes;
+  }
+
+  @Override
+  public int compareTo(Swimmer otherSwimmer) {
+    double avgTimeThis = this.freestyle50mTimes
+        .stream()
+        .mapToDouble(Double :: doubleValue)
+        .average()
+        .orElse(0.0);
+
+    double avgTimeOther = otherSwimmer.freestyle50mTimes
+        .stream()
+        .mapToDouble(Double :: doubleValue)
+        .average()
+        .orElse(0.0);
+
+    return Double.compare(avgTimeThis, avgTimeOther);
+  }
+}
